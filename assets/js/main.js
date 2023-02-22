d65ae54438d279d43f4e2d309fae791659b3f92a
radowanTri <radowan.bhuiyan.tri@gmail.com>
2023-02-22 17:26:29 +0600

    final jack

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,29 @@ $(document).ready(function($) {
             },
             srcAction: 'iframe_src',
         }
-    });
+     });
+    $('.news-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>',
+        ],
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            
+            }
+        }
+    })
 
 
 
